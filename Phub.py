fdee3f60a056282e3d75fc5178ea0c0f31152951
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-21 17:55:16 +0700

    Update Phub.py

--- Phub.py
+++ Phub.py
@@ -122,6 +122,47 @@ async def sarch(_,message):
     )
     new_db={"result":res,"curr_page":0}
     db[message.chat.id] = new_db
+
+@app.on_message(filters.command("cari") & filters.group & ~ filters.edited)
+async def sarch(_,message):
+    m = await message.reply_text("Mendapatkan Hasil.....")
+    search = message.text
+    try:
+        resp = await pornhub(search,thumbsize="large")
+        res = resp.result
+    except:
+        await m.edit("Tidak Menemukan... Coba lagi")
+        return
+    if not resp.ok:
+        await m.edit("Tidak Menemukan... Coba lagi")
+        return
+    resolt = f"""
+**🏷JUDUL:** {res[0].title}
+**⏰DURASI:** {res[0].duration}
+**👁‍🗨DILIHAT:** {res[0].views}
+**🌟RATING:** {res[0].rating}"""
+    await m.delete()
+    m = await message.reply_photo(
+        photo=res[0].thumbnails[0].src,
+        caption=resolt,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("▶️NEXT",
+                                         callback_data="next"),
+                    InlineKeyboardButton("🗑HAPUS",
+                                         callback_data="delete"),
+                ],
+                [
+                    InlineKeyboardButton("📥DOWNLOAD",
+                                         callback_data="dload")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+    new_db={"result":res,"curr_page":0}
+    db[message.chat.id] = new_db
     
  # Next Button--------------------------------------------------------------------------
 @app.on_callback_query(filters.regex("next"))
@@ -293,46 +334,5 @@ async def callback_query_dl(_, query):
 @app.on_callback_query(filters.regex("delete"))
 async def callback_query_delete(_, query):
     await query.message.delete()
-
-@app.on_message(filters.command("cari") & filters.group & ~ filters.edited)
-async def sarch(_,message):
-    m = await message.reply_text("Mendapatkan Hasil.....")
-    search = message.text
-    try:
-        resp = await pornhub(search,thumbsize="large")
-        res = resp.result
-    except:
-        await m.edit("Tidak Menemukan... Coba lagi")
-        return
-    if not resp.ok:
-        await m.edit("Tidak Menemukan... Coba lagi")
-        return
-    resolt = f"""
-**🏷JUDUL:** {res[0].title}
-**⏰DURASI:** {res[0].duration}
-**👁‍🗨DILIHAT:** {res[0].views}
-**🌟RATING:** {res[0].rating}"""
-    await m.delete()
-    m = await message.reply_photo(
-        photo=res[0].thumbnails[0].src,
-        caption=resolt,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("▶️NEXT",
-                                         callback_data="next"),
-                    InlineKeyboardButton("🗑HAPUS",
-                                         callback_data="delete"),
-                ],
-                [
-                    InlineKeyboardButton("📥DOWNLOAD",
-                                         callback_data="dload")
-                ]
-            ]
-        ),
-        parse_mode="markdown",
-    )
-    new_db={"result":res,"curr_page":0}
-    db[message.chat.id] = new_db
-    
+ 
 app.run()
